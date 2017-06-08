c3f66b28ed0ab8aa1d8524216602f80ada944045
Jason Lam <jasonlam87456@gmail.com>
2017-06-07 17:59:38 -0700

    add sql additions

---
+++ phase2_sql.sql
@@ -0,0 +1,39 @@
+with overall_table as(
+  select i.sku,u.state,sum(i.price) as amount  
+  from items i
+  inner join carts c on (c.id = i.id and c.purchase_date is not null)
+  inner join product p on (i.sku = p.sku )
+  inner join users u on (c.name = u.name)
+  group by i.sku,u.state
+),
+top_state as(
+  select state, sum(amount) as dollar from (
+  select state, amount from overall_table
+  UNION ALL
+  select state as state, 0.0 as amount from state
+  ) as state_union
+  group by state order by dollar desc
+),
+top_n_state as(
+  select row_number() over(order by dollar desc) as state_order, state, dollar from top_state
+),
+top_prod as(
+  select sku, sum(amount) as dollar from (
+  select sku, amount from overall_table
+  UNION ALL
+  select sku as sku, 0.0 as amount from product
+  ) as product_union
+  group by sku order by dollar desc
+),
+top_n_prod as (
+  select row_number() over(order by dollar desc) as product_order, sku, dollar from top_prod
+)
+select ts.state, s.state, tp.sku, pr.product_name, COALESCE(ot.amount, 0.0) as cell_sum, ts.dollar as state_sum, tp.dollar as product_sum
+  from top_n_prod tp CROSS JOIN top_n_state ts 
+  LEFT OUTER JOIN overall_table ot 
+  ON ( tp.sku = ot.sku and ts.state = ot.state)
+  inner join state s ON ts.state = s.state
+  inner join product pr ON tp.sku = pr.sku
+  order by ts.state_order, tp.product_order
+
+
---
+++ state.sql
@@ -0,0 +1,60 @@
+create table state (
+    id serial primary key,
+    state text unique not null);
+INSERT INTO state (state) VALUES ('AL');
+INSERT INTO state (state) VALUES ('AK');
+INSERT INTO state (state) VALUES ('AZ');
+INSERT INTO state (state) VALUES ('AR');
+INSERT INTO state (state) VALUES ('CA');
+INSERT INTO state (state) VALUES ('CO');
+INSERT INTO state (state) VALUES ('CT');
+INSERT INTO state (state) VALUES ('DE');
+INSERT INTO state (state) VALUES ('FL');
+INSERT INTO state (state) VALUES ('GA');
+INSERT INTO state (state) VALUES ('HI');
+INSERT INTO state (state) VALUES ('ID');
+INSERT INTO state (state) VALUES ('IL');
+INSERT INTO state (state) VALUES ('IN');
+INSERT INTO state (state) VALUES ('IA');
+INSERT INTO state (state) VALUES ('KS');
+INSERT INTO state (state) VALUES ('KY');
+INSERT INTO state (state) VALUES ('LA');
+INSERT INTO state (state) VALUES ('ME');
+INSERT INTO state (state) VALUES ('MD');
+INSERT INTO state (state) VALUES ('MA');
+INSERT INTO state (state) VALUES ('MI');
+INSERT INTO state (state) VALUES ('MN');
+INSERT INTO state (state) VALUES ('MS');
+INSERT INTO state (state) VALUES ('MO');
+INSERT INTO state (state) VALUES ('MT');
+INSERT INTO state (state) VALUES ('NE');
+INSERT INTO state (state) VALUES ('NV');
+INSERT INTO state (state) VALUES ('NH');
+INSERT INTO state (state) VALUES ('NJ');
+INSERT INTO state (state) VALUES ('NM');
+INSERT INTO state (state) VALUES ('NY');
+INSERT INTO state (state) VALUES ('NC');
+INSERT INTO state (state) VALUES ('ND');
+INSERT INTO state (state) VALUES ('OH');
+INSERT INTO state (state) VALUES ('OK');
+INSERT INTO state (state) VALUES ('OR');
+INSERT INTO state (state) VALUES ('PA');
+INSERT INTO state (state) VALUES ('RI');
+INSERT INTO state (state) VALUES ('SC');
+INSERT INTO state (state) VALUES ('SD');
+INSERT INTO state (state) VALUES ('TN');
+INSERT INTO state (state) VALUES ('TX');
+INSERT INTO state (state) VALUES ('UT');
+INSERT INTO state (state) VALUES ('VT');
+INSERT INTO state (state) VALUES ('VA');
+INSERT INTO state (state) VALUES ('WA');
+INSERT INTO state (state) VALUES ('WV');
+INSERT INTO state (state) VALUES ('WI');
+INSERT INTO state (state) VALUES ('WY');
+INSERT INTO state (state) VALUES ('DC');
+
+
+
+
+
+
